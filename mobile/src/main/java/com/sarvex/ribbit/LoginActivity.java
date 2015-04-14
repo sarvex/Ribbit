9ff38cab6506227932740ae873ac6b3c7d417601
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2015-04-15 00:45:14 +0530

    Added Login Screen

--- mobile/src/main/java/com/sarvex/ribbit/LoginActivity.java
+++ mobile/src/main/java/com/sarvex/ribbit/LoginActivity.java
@@ -1,17 +1,49 @@
 package com.sarvex.ribbit;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class LoginActivity extends ActionBarActivity {
+public class LoginActivity extends Activity {
+
+    private Button loginButton;
+    private Button signUpButton;
+
+    private EditText usernameField;
+    private EditText emailField;
+    private EditText passwordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setEnabled(false);
+
+        signUpButton = (Button) findViewById(R.id.signUpButton);
+        signUpButton.setEnabled(false);
+
+        usernameField = (EditText) findViewById(R.id.userNameField);
+        usernameField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                loginButton.setEnabled(true);
+                return true;
+            }
+        });
+
+        emailField = (EditText) findViewById(R.id.emailField);
+        emailField.setEnabled(false);
+
+        passwordField = (EditText) findViewById(R.id.passwordField);
+
     }
 
     @Override
