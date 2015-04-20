b64ea3ab6f597f62429e9b11c7a56d980ae02460
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2015-04-20 11:17:41 +0530

    Added Butterknife Injection

--- mobile/src/main/java/com/sarvex/ribbit/LoginActivity.java
+++ mobile/src/main/java/com/sarvex/ribbit/LoginActivity.java
@@ -4,133 +4,141 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+import butterknife.OnEditorAction;
+
 
 public class LoginActivity extends Activity {
 
-    private Button loginButton;
-    private Button signUpButton;
+    @InjectView(R.id.loginButton)
+    Button loginButton;
 
-    private EditText usernameField;
-    private EditText emailField;
-    private EditText passwordField;
+    @InjectView(R.id.signupButton)
+    Button signupButton;
+
+    @InjectView(R.id.userNameField)
+    EditText usernameField;
+
+    @InjectView(R.id.passwordField)
+    EditText passwordField;
+
+    @InjectView(R.id.emailField)
+    EditText emailField;
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        ButterKnife.reset(this);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+
         setContentView(R.layout.activity_login);
+        ButterKnife.inject(this);
 
-
-        usernameField = (EditText) findViewById(R.id.userNameField);
-        usernameField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
-                loginButton.setEnabled(true);
-                return true;
-            }
-        });
-
-        passwordField = (EditText) findViewById(R.id.passwordField);
-
-        emailField = (EditText) findViewById(R.id.emailField);
-        emailField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
-                boolean status = (emailField.getText().toString().trim().length() > 0);
-                loginButton.setEnabled(!status);
-                signUpButton.setEnabled(status);
-                return false;
-            }
-        });
-
-        loginButton = (Button) findViewById(R.id.loginButton);
         loginButton.setEnabled(false);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String username = usernameField.getText().toString().trim();
-                String password = passwordField.getText().toString().trim();
+        signupButton.setEnabled(false);
+    }
 
-                if (username.isEmpty() || password.isEmpty()) {
-                    new AlertDialog.Builder(LoginActivity.this)
-                            .setMessage(R.string.login_error_message)
-                            .setTitle(R.string.login_error_title)
-                            .setPositiveButton(android.R.string.ok, null).create();
-                } else {
-                    ParseUser.logInInBackground(username, password, new LogInCallback() {
-                        @Override
-                        public void done(ParseUser user, ParseException exception) {
-                            if (user != null) {
-                                startActivity(new Intent(LoginActivity.this, MainActivity.class)
-                                        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                                        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
-                            } else {
-                                new AlertDialog.Builder(LoginActivity.this)
-                                        .setMessage(exception.getMessage())
-                                        .setTitle(R.string.login_error_title)
-                                        .setPositiveButton(android.R.string.ok, null).create();
-                            }
-                        }
-                    });
+    @OnEditorAction(R.id.userNameField)
+    public boolean enableLogin() {
+        loginButton.setEnabled(true);
+        return true;
+    }
+
+    @OnEditorAction(R.id.emailField)
+    public boolean enableSignup() {
+        boolean status = (emailField.getText().toString().trim().length() > 0);
+        loginButton.setEnabled(!status);
+        signupButton.setEnabled(status);
+        return status;
+    }
+
+    @OnClick(R.id.loginButton)
+    public void login() {
+        String username = usernameField.getText().toString().trim();
+        String password = passwordField.getText().toString().trim();
+
+        if (username.isEmpty() || password.isEmpty()) {
+            new AlertDialog.Builder(LoginActivity.this)
+                    .setMessage(R.string.login_error_message)
+                    .setTitle(R.string.login_error_title)
+                    .setPositiveButton(android.R.string.ok, null).create();
+        } else {
+            setProgressBarIndeterminateVisibility(true);
+            ParseUser.logInInBackground(username, password, new LogInCallback() {
+                @Override
+                public void done(ParseUser user, ParseException exception) {
+                    setProgressBarIndeterminateVisibility(false);
+                    if (user != null) {
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class)
+                                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                                .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+                    } else {
+                        new AlertDialog.Builder(LoginActivity.this)
+                                .setMessage(exception.getMessage())
+                                .setTitle(R.string.login_error_title)
+                                .setPositiveButton(android.R.string.ok, null).create();
+                    }
                 }
-            }
-        });
+            });
+        }
+    }
 
-        signUpButton = (Button) findViewById(R.id.signUpButton);
-        signUpButton.setEnabled(false);
-        signUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String username = usernameField.getText().toString().trim();
-                String password = passwordField.getText().toString().trim();
-                String email = emailField.getText().toString().trim();
+    @OnClick(R.id.signupButton)
+    public void signup() {
+        String username = usernameField.getText().toString().trim();
+        String password = passwordField.getText().toString().trim();
+        String email = emailField.getText().toString().trim();
 
-                if (username.isEmpty() || password.isEmpty() || email.isEmpty()) {
+        if (username.isEmpty() || password.isEmpty() || email.isEmpty()) {
 
-                    new AlertDialog.Builder(LoginActivity.this)
-                            .setMessage(R.string.signup_error_message)
-                            .setTitle(R.string.signup_error_title)
-                            .setPositiveButton(android.R.string.ok, null).create();
+            new AlertDialog.Builder(LoginActivity.this)
+                    .setMessage(R.string.signup_error_message)
+                    .setTitle(R.string.signup_error_title)
+                    .setPositiveButton(android.R.string.ok, null).create();
 
-                } else {
-                    // create a new user
-                    ParseUser user = new ParseUser();
-                    user.setUsername(username);
-                    user.setPassword(password);
-                    user.setEmail(email);
-                    user.signUpInBackground(new SignUpCallback() {
-                        @Override
-                        public void done(ParseException exception) {
-                            if (exception == null) {
-                                startActivity(new Intent(LoginActivity.this, MainActivity.class)
-                                        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                                        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
-                            } else {
-                                new AlertDialog.Builder(LoginActivity.this)
-                                        .setMessage(exception.getMessage())
-                                        .setTitle(R.string.signup_error_title)
-                                        .setPositiveButton(android.R.string.ok, null).create();
-                            }
-                        }
-                    });
-
+        } else {
+            // create a new user
+            ParseUser user = new ParseUser();
+            user.setUsername(username);
+            user.setPassword(password);
+            user.setEmail(email);
+            user.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException exception) {
+                    if (exception == null) {
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class)
+                                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                                .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+                    } else {
+                        new AlertDialog.Builder(LoginActivity.this)
+                                .setMessage(exception.getMessage())
+                                .setTitle(R.string.signup_error_title)
+                                .setPositiveButton(android.R.string.ok, null).create();
+                    }
                 }
-            }
-        });
+            });
 
-
+        }
     }
 
     @Override
